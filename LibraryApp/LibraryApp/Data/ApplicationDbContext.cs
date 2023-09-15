14e44f0027404a3d10725b02788971c710414357
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2023-09-15 12:36:10 +0300

    Installed and configured EF Core

---
+++ LibraryApp/LibraryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryApp.Data
+{
+	using Microsoft.EntityFrameworkCore;
+
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+	}
+}
