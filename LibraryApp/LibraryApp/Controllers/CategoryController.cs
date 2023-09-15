fcd69cccdbe0300a80adc30bcb32cff9850cdd39
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2023-09-15 18:05:14 +0300

    Implemented Category list view

---
+++ LibraryApp/LibraryApp/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers
+{
+	using Data;
+
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult Index()
+		{
+			var categories = _db.Categories.ToList();
+			return View(categories);
+		}
+	}
+}
--- LibraryApp/LibraryApp/Program.cs
+++ LibraryApp/LibraryApp/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Category}/{action=Index}/{id?}");
 
 app.Run();
