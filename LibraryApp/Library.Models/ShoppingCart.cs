3fe787a61f2fd1cf810feefd302d21990c533a08
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2023-11-07 17:59:59 +0200

    Created model and migrations for ShoppingCart

---
+++ LibraryApp/Library.Models/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Models
+{
+	public class ShoppingCart
+	{
+		public int Id { get; set; }
+		public int ProductId { get; set; }
+		public required string ApplicationUserId { get; set; }
+		
+		[Range(1, 100, ErrorMessage = "Please enter a value between 1 and 100")]
+		public int Count { get; set; }
+
+		[ValidateNever]
+		[ForeignKey("ProductId")]
+		public Product Product { get; set; }
+
+		[ValidateNever]
+		[ForeignKey("ApplicationUserId")]
+		public ApplicationUser ApplicationUser { get; set; }
+	}
+}
