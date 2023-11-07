af9d41521f0f25996311917bb6fd3eaab3b24bd6
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2023-11-07 18:08:41 +0200

    Created and registered ApplicationUser repository

---
+++ LibraryApp/Library.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.DataAccess.Data;
+using Library.DataAccess.Repository.Interfaces;
+using Library.Models;
+
+namespace Library.DataAccess.Repository
+{
+	public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+	{
+		public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+		{
+		}
+	}
+}
--- LibraryApp/LibraryApp/Program.cs
+++ LibraryApp/LibraryApp/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+builder.Services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
 
 var app = builder.Build();
 
