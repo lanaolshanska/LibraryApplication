235110b9ef71655cfae82d6872cb4a2c003a3c84
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2023-11-11 20:06:03 +0200

    Added ShoppingCart UI

---
+++ LibraryApp/LibraryApp/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,14 @@
+﻿using Library.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Areas.Customer.Controllers
+{
+	[Area(Role.Customer)]
+	public class ShoppingCartController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
