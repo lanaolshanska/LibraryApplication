f71f8242bf9449f5a2cd392e5e9e885cdabb3c71
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2024-02-01 19:01:51 +0400

    Included ignored js file to toggle company dropdown

---
+++ LibraryApp/LibraryApp/wwwroot/js/toggleCompanySelect.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    toggleCompanySelect();
+
+    $('#Input_Role').change(function () {
+        toggleCompanySelect();
+    })
+});
+
+function toggleCompanySelect() {
+    var selection = $('#Input_Role Option:Selected').text();
+    if (selection == 'Company') {
+        $('#Input_CompanyId').show();
+    }
+    else {
+        $('#Input_CompanyId').hide();
+    }
+}
